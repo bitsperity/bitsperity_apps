98d73344f3fe5fd79c6c965b62dd407ef007f2b1
rafiki71 <rafiki71@bitsperity.dev>
2025-05-29 12:30:03 +0200

    Clean up repository: Remove 16 unused test/debug/legacy files (~54KB) - Repository is now cleaner and only contains active files

--- bitsperity-beacon/exports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-# Bitsperity Beacon exports for other apps
-export APP_BITSPERITY_BEACON_API_URL="http://bitsperity-beacon_beacon-api_1:8000"
-export APP_BITSPERITY_BEACON_WS_URL="ws://bitsperity-beacon_beacon-api_1:8000/api/v1/ws"
-export APP_BITSPERITY_BEACON_DISCOVERY_API="http://bitsperity-beacon_beacon-api_1:8000/api/v1/services/discover" 
